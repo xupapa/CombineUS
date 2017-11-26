3ef8bab63eef7027dd2b7bf639b81ad2025309f8
shenchenyang <907693148@qq.com>
2017-11-26 23:57:05 +0800

    ng 测试
    测试

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  isCollapsed = false;
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
 }
